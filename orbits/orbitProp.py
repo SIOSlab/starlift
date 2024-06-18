2485ffd5ac1a73c0fdce89f35be169f204d1a34d
annaboese <aeb293@cornell.edu>
2024-06-18 14:50:27 -0400

    Attempted to animate moon and Earth orbit (in progress)

--- orbits/orbitProp.py
+++ orbits/orbitProp.py
@@ -42,6 +42,40 @@ statesCRTBP, timesCRTBP = orbitEOMProp.statePropCRTBP(freeVar_CRTBP, mu_star)
 posCRTBP = statesCRTBP[:, 0:3]
 velCRTBP = statesCRTBP[:, 3:6]
 
+# # preallocate space
+# r_PEM_CRTBP = np.zeros([len(timesCRTBP), 3])
+# r_SunEM_CRTBP = np.zeros([len(timesCRTBP), 3])
+# r_EarthEM_CRTBP = np.zeros([len(timesCRTBP), 3])
+# r_MoonEM_CRTBP = np.zeros([len(timesCRTBP), 3])
+#
+# # sim time in mjd
+# timesCRTBP_mjd = timesCRTBP + t_mjd
+
+# DCM for G frame and I frame
+C_B2G = frameConversion.body2geo(t_mjd, t_mjd, mu_star)
+C_G2B = C_B2G.T
+
+# # This for loop exists to plot the Earth and the Moon around the CRTBP orbit, but it takes a LONG time...
+# for ii in np.arange(len(timesCRTBP)):
+#     time = timesCRTBP_mjd[ii]
+#
+#     # positions of the Sun, Moon, and EM barycenter relative SS barycenter in H frame
+#     r_MoonO = get_body_barycentric_posvel('Moon', time)[0].get_xyz().to('AU').value
+#     EMO = get_body_barycentric_posvel('Earth-Moon-Barycenter', time)
+#     r_EMO = EMO[0].get_xyz().to('AU').value
+#
+#     # convert from H frame to GCRS frame
+#     r_EMG = frameConversion.icrs2gcrs(r_EMO * u.AU, time)
+#     r_MoonG = frameConversion.icrs2gcrs(r_MoonO * u.AU, time)
+#
+#     # change the origin to the EM barycenter, G frame
+#     r_EarthEM = -r_EMG
+#     r_MoonEM = r_MoonG - r_EMG
+#
+#     # convert from G frame to I frame
+#     r_EarthEM_CRTBP[ii, :] = C_G2B @ r_EarthEM.to('AU')
+#     r_MoonEM_CRTBP[ii, :] = C_G2B @ r_MoonEM.to('AU')
+
 # convert position from I frame to H frame
 pos_H, vel_H, Tp_dim = orbitEOMProp.convertIC_R2H(posCRTBP[0], velCRTBP[0], t_mjd, timesCRTBP[-1], mu_star)
 
@@ -62,9 +96,6 @@ r_MoonEM_r = np.zeros([len(timesFF), 3])
 # sim time in mjd
 timesFF_mjd = timesFF + t_mjd
 
-# DCM for G frame and I frame
-C_B2G = frameConversion.body2geo(t_mjd,t_mjd,mu_star)
-C_G2B = C_B2G.T
 for ii in np.arange(len(timesFF)):
     time = timesFF_mjd[ii]
     
@@ -92,21 +123,44 @@ for ii in np.arange(len(timesFF)):
     r_EarthEM_r[ii, :] = C_G2B@r_EarthEM.to('AU')
     r_MoonEM_r[ii, :] = C_G2B@r_MoonEM.to('AU')
 
-
 # animate the CRTBP orbit
 P = 50  # number of points plotted per frame
-def update(num, data, line):
-    line.set_data(data[0:2, 0:num*P])
-    line.set_3d_properties(data[2, 0:num*P])
+def animate(num, data_CRTBP, line_CRTBP):
+    line_CRTBP.set_data(data_CRTBP[0:2, 0:num*P])
+    line_CRTBP.set_3d_properties(data_CRTBP[2, 0:num*P])
+    # line_Earth.set_data(data_Earth[0:2, 0:num*P])
+    # line_Earth.set_3d_properties(data_Earth[2, 0:num*P])
+    # line_Moon.set_data(data_Moon[0:2, 0:num*P])
+    # line_Moon.set_3d_properties(data_Moon[2, 0:num*P])
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-data_CRTBP = np.array([posCRTBP[:, 0], posCRTBP[:, 1], posCRTBP[:, 2]])
-N = len(data_CRTBP[0])  # number of frames in animation
-line_CRTBP, = ax.plot(data_CRTBP[0, 0:1], data_CRTBP[1, 0:1], data_CRTBP[2, 0:1])
 
-ani = animation.FuncAnimation(fig, update, frames=N//P, fargs=(data_CRTBP, line_CRTBP), interval=1, repeat=False,
-                              cache_frame_data=False)
+N_CRTBP = len(posCRTBP[:, 0])  # number of frames in animation
+data_CRTBP = np.array([posCRTBP[:, 0], posCRTBP[:, 1], posCRTBP[:, 2]])
+line_CRTBP, = ax.plot(data_CRTBP[0, 0:1], data_CRTBP[1, 0:1], data_CRTBP[2, 0:1])
+# data_Earth = np.array([r_EarthEM_CRTBP[:, 0], r_EarthEM_CRTBP[:, 1], r_EarthEM_CRTBP[:, 2]])
+# line_Earth, = ax.plot(data_Earth[0, 0:1], data_Earth[1, 0:1], data_Earth[2, 0:1])
+# data_Moon = np.array([r_MoonEM_CRTBP[:, 0], r_MoonEM_CRTBP[:, 1], r_MoonEM_CRTBP[:, 2]])
+# line_Moon, = ax.plot(data_Moon[0, 0:1], data_Moon[1, 0:1], data_Moon[2, 0:1])
+
+# # Anna's debugging
+# print(type(data_CRTBP))
+# print(data_CRTBP)
+# print(type(line_CRTBP))
+# print(line_CRTBP)
+# print(type(data_Earth))
+# print(data_Earth)
+# print(type(line_Earth))
+# print(line_Earth)
+# print(type(data_Moon))
+# print(data_Moon)
+# print(type(line_Moon))
+# print(line_Moon)
+
+
+ani_CRTBP = animation.FuncAnimation(fig, animate, frames=N_CRTBP//P, fargs=(data_CRTBP, line_CRTBP),
+                                    interval=1, repeat=False, cache_frame_data=False)
 
 """
 # plot CRTBP and FF solutions
