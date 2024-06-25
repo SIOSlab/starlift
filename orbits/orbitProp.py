Fixed sign errors in initial conditions conversion function

orbits/orbitProp.py
@@ -11,7 +11,11 @@ import astropy.units as u
 import astropy.constants as const
 from matplotlib import pyplot as plt
 from matplotlib import animation
-# sys.path.insert(1, 'tools')
+#sys.path.insert(1, 'tools')
+#import unitConversion
+#import frameConversion
+#import orbitEOMProp
+#import plot_tools
 import tools.unitConversion as unitConversion
 import tools.frameConversion as frameConversion
 import tools.orbitEOMProp as orbitEOMProp
@@ -84,6 +88,7 @@ C_G2B = C_B2G.T
 # Convert position from I frame to H frame [AU]
 pos_H, vel_H, Tp_dim = orbitEOMProp.convertIC_R2H(posCRTBP[0], velCRTBP[0], t_mjd, timesCRTBP[-1], mu_star)
 
+breakpoint()
 # Define the initial state array
 state0 = np.append(np.append(pos_H.value, vel_H.value), days)   # Tp_dim.value
 