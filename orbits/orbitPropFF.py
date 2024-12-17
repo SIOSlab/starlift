Fixed unit conversion errors in inert2rotV and rot2inertV

orbits/orbitPropFF.py
@@ -77,17 +77,14 @@ if theta > np.pi/2:
 rot_matrix = frameConversion.rot(theta, 3)
 IC[3:6] = rot_matrix @ vO  # Canonical, I frame
 
-# Convert IC to dimensional, rotating frame (for STK) (THIS MIGHT BE WRONG)
-pos_dim = unitConversion.convertPos_to_dim(IC[0:3]).to('km')  # I frame, dimensional
-vel_dim = unitConversion.convertVel_to_dim(IC[3:6]).to('km/s')  # I frame, dimensional
+# Convert IC to dimensional, rotating frame (for STK)
 C_I2R = frameConversion.inert2rot(t_start, t_start)
-pos_dimrot = C_I2R @ pos_dim  # R frame
-# vel_dimrot = C_I2R @ vel_dim  # R frame
-vel_dimrot = frameConversion.inert2rotV(pos_dim, vel_dim, 0)  # R frame
+pos_canrot = C_I2R @ IC[0:3]  # Canonical, R frame
+vel_canrot = frameConversion.inert2rotV(pos_canrot, IC[3:6], 0)  # Canonical, R frame
+pos_dimrot = unitConversion.convertPos_to_dim(pos_canrot).to('km')  # R frame, dimensional
+vel_dimrot = unitConversion.convertVel_to_dim(vel_canrot).to('km/s')  # R frame, dimensional
 print('Dimensional [km] position IC in the rotating frame: ', pos_dimrot)
 print('Dimensional [km/s] velocity IC in the rotating frame: ', vel_dimrot)
-
-breakpoint()
 
 # Convert ICs to H frame (AU and AU/d) from I frame (canonical)
 pos_H, vel_H = frameConversion.convertSC_I2H(IC[0:3], IC[3:6], t_start, C_I2G)

orbits/tools/frameConversion.py
@@ -491,62 +491,51 @@ def rot2inertV(rR, vR, t_norm):
     """Convert velocity from rotating frame to inertial frame
 
     Args:
-        rR (float nx3 array):
-            Rotating frame position vectors
-        vR (float nx3 array):
-            Rotating frame velocity vectors
-        t_norm (float):
-            Normalized time units for current epoch
+      rR (float nx3 array):
+        Rotating frame position vectors in canonical units
+      vR (float nx3 array):
+        Rotating frame velocity vectors in canonical units
+      t_norm (float):
+        Normalized time units for current epoch
+
     Returns:
-        float nx3 array:
-            Inertial frame velocity vectors
+      float nx3 array:
+        Inertial frame velocity vectors in canonical units
     """
 
+    e3 = np.array([0, 0, 1])
     if rR.shape[0] == 3 and len(rR.shape) == 1:
-        At = rot(t_norm, 3).T
-        drR = np.array([-rR[1], rR[0], 0])
-        vI = np.dot(At, vR.T) + np.dot(At, drR.T)
+        vI = vR + np.cross(e3, rR)
     else:
-        vI = np.zeros([len(rR), 3])
-        for t in range(len(rR)):
-            At = rot(t_norm, 3).T
-            drR = np.array([-rR[t, 1], rR[t, 0], 0])
-            vI[t, :] = np.dot(At, vR[t, :].T) + np.dot(At, drR.T)
+        vR = np.zeros([len(t_norm), 3])
+        for t in range(len(t_norm)):
+            vI[t, :] = vR[t, :] + np.cross(e3, rR[t, :])
+
     return vI
-    
 
 def inert2rotV(rR, vI, t_norm):
     """Convert velocity from inertial frame to rotating frame
 
     Args:
-        rR (float nx3 array):
-            Rotating frame position vector
-        vI (float nx3 array):
-            Inertial frame velocity vector
-        t_norm (float):
-            Normalized time units for current epoch
+      rR (float nx3 array):
+        Rotating frame position vectors in canonical units
+      vI (float nx3 array):
+        Inertial frame velocity vectors in canonical units
+      t_norm (float):
+        Normalized time units for current epoch
+
     Returns:
-        float nx3 array:
-            Rotating frame velocity vectors
+      float nx3 array:
+        Rotating frame velocity vectors in canonical units
     """
 
-    # if t_norm.size == 1:
-    #     t_norm = np.array([t_norm])
-    # vR = np.zeros([len(t_norm), 3])
-    # for t in range(len(t_norm)):
-    #     At = rot(t_norm[t], 3)
-    #     vR[t, :] = np.dot(At, vI[t, :].T) + np.array([rR[t, 1], -rR[t, 0], 0]).T
-
+    e3 = np.array([0, 0, 1])
     if rR.shape[0] == 3 and len(rR.shape) == 1:
-        At = rot(t_norm, 3).T
-        drI = np.array([rR[1].value, -rR[0].value, 0])
-        vR = np.dot(At, vI.value.T) + np.dot(At, drI.T)
+        vR = vI - np.cross(e3, rR)
     else:
-        vR = np.zeros([len(rR), 3])
-        for t in range(len(rR)):
-            At = rot(t_norm, 3).T
-            drI = np.array([rR[t, 1].value, -rR[t, 0].value, 0])
-            vR[t, :] = np.dot(At, vI[t, :].value.T) + np.dot(At, drI.T)
+        vR = np.zeros([len(t_norm), 3])
+        for t in range(len(t_norm)):
+            vR[t, :] = vI[t, :] - np.cross(e3, rR[t, :])
 
     return vR
 