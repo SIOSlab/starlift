Fixing sign errors. Works without periodicity constraint

orbits/ephemerisModel.py
@@ -28,7 +28,7 @@ GM = np.array([gmMoon, gmEarth, gmSun])
 
 orbs = 1
 t_equinox = Time(51544.5, format='mjd', scale='utc')
-t_veq = t_equinox + 79.3125*u.d  # + 1*u.yr/4
+t_veq = t_equinox + 79.3125*u.d
 #t_start = Time(57727, format='mjd', scale='utc')
 t_start = Time(58070, format='mjd', scale='utc')
 days = 14*orbs
@@ -44,7 +44,8 @@ omega_m = spice.oscltx(rvMoon,et_start,gmEarth)[-1]
 
 # Initial condition in canonical units in rotating frame R [pos, vel]
 #IC = [1.011035058929108, 0, -0.173149999840112, 0, -0.078014276336041, 0,  1.3632096570/2]  # L2, 5.92773293-day period
-IC = [0.9624690577, 0, 0, 0, 0.7184165432, 0, 0.2230147974/2]  # DRO, 0.9697497-day period
+IC = [1.01103506347211, 0, -0.17315001039682773, 0, -0.07801414771853428, 0, 1.363209636932144/2]    #1E-7 error version of ^
+#IC = [0.9624690577, 0, 0, 0, 0.7184165432, 0, 0.2230147974/2]   # DRO, 0.9697497-day period
 #IC = [0.429519110229904, 0, 0, 0, 1.440796689672539, 0, 3.051133070334277]
 #IC = [1.165130674583613, 0, -0.110699848144854, 0, 0.201519926517907, 0, 1.652428300688599]
 #IC = [1.114959432252717, 0, 0.027057507726036, 0, 0.191674660415012, 0, 3.403442494940593/2]   # matlab
@@ -63,9 +64,10 @@ while error > eps and ctr < max_iter:
     X = X - dFx.T @ (np.linalg.inv(dFx @ dFx.T) @ Fx)
 
     ctr = ctr + 1
+    print('Error is: '+str(error))
 
 IC = np.array([X[0], 0, X[1], 0, X[2], 0, 2*X[3]])  # Canonical, rotating frame
-
+print('Number of attempts: '+str(ctr))
 # Propagate the dynamics (states in AU or AU/day, times in days starting from 0)
 freeVar0CRTBP_R = X.copy()
 freeVar0CRTBP_R[-1] = 2*freeVar0CRTBP_R[-1]*orbs
@@ -78,7 +80,14 @@ timesCRTBP_d = unitConversion.convertTime_to_dim(timesCRTBP_R).to('d')
 timesCRTBP_mjd = t_start + timesCRTBP_d
 etCRTBP_mjd = spice.str2et(timesCRTBP_mjd.iso)
 
-N = 15
+posCRTBP_I_dim = np.zeros((len(etCRTBP_mjd), 3))
+for ii in np.arange(len(etCRTBP_mjd)):
+    Crv_R2I = spice.sxform('MCR','MCI',etCRTBP_mjd[ii])
+    state_R = np.append(posCRTBP_R_dim[ii], velCRTBP_R_dim[ii])
+    state_I = Crv_R2I@state_R
+    posCRTBP_I_dim[ii,:] = state_I[0:3]
+
+N = 9*orbs
 dt_int = (timesCRTBP_d[-1]-timesCRTBP_d[0])/(N-1)
 taus = Time(np.zeros(N), format='mjd', scale='utc')
 posvel = np.array([])
@@ -111,6 +120,10 @@ ax1 = plt.figure().add_subplot(projection='3d')
 for ii in np.arange(N):
     ax1.scatter(posvel[ii,0], posvel[ii,1], posvel[ii,2], c='g', marker='o')
 ax1.plot(posCRTBP_R_dim[:,0], posCRTBP_R_dim[:,1], posCRTBP_R_dim[:,2], 'r', label='CRTBP')
+ax1.set_xlabel('X [km]')
+ax1.set_ylabel('Y [km]')
+ax1.set_zlabel('Z [km]')
+ax1.set_title('Patch Points in Moon Centered Rotating Frame')
 
 # Convert to MCI frame
 initialEphmerisEpoch = spice.str2et(t_start.iso)
@@ -122,29 +135,75 @@ for ii in np.arange(len(times_dim)):
     initialEphemerisMCI[ii,:] = Crv_R2I[ii,:,:] @ posvel[ii]
     
 positionTolerance = 0.01    # km
-velocityTolerance = 0.0001  # km/s
-#velocityTolerance = 0.001  # km/s
+velocityTolerance = 0.0001*orbs  # km/s
 
 #correctedInitialEpoches, correctedInitialStates, exitflag = ms.multipleShootingI(initialEphmerisEpoches, initialEphemerisMCI, positionTolerance, velocityTolerance, GM)
 correctedInitialEpoches, correctedInitialStates, exitflag = ms.multipleShootingR(initialEphmerisEpoches, initialEphemerisMCI, positionTolerance, velocityTolerance, GM, omega_m)
 #correctedInitialEpoches, correctedInitialStates, exitflag = ms.multipleShootingR(initialEphmerisEpoches, posvel, positionTolerance, velocityTolerance, GM, omega_m)
 
-# rewrite this section to integrate in I and then rotate to R
-Crv_I2R = spice.sxform('MCI','MCR',correctedInitialEpoches)
-rotatedInitialStates = np.zeros((N,6))
-for ii in np.arange(N):
-    rotatedInitialStates[ii,:] = Crv_I2R[ii,:,:] @ correctedInitialStates[ii,:]
-    
+# Plot in MCI and MCR
 ax10 = plt.figure().add_subplot(projection='3d')
-for ii in np.arange(0,N-1):
+ax11 = plt.figure().add_subplot(projection='3d')
+for ii in np.arange(N-1):
     Ts = correctedInitialEpoches[ii:ii+2]
-    times, states = ms.statePropFFR(Ts, rotatedInitialStates[ii,:], GM, omega_m)
+    times, states = ms.statePropFFI(Ts, correctedInitialStates[ii,:], GM)
     
-    ax10.plot(states[:, 0], states[:, 1], states[:, 2], 'b', label='Multi Segment')
-    ax10.scatter(rotatedInitialStates[ii,0], rotatedInitialStates[ii,1], rotatedInitialStates[ii,2], c='g', marker='o')
-    ax10.scatter(rotatedInitialStates[ii+1,0], rotatedInitialStates[ii+1,1], rotatedInitialStates[ii+1,2], c='y', marker='*')
+    ax11.plot(states[:, 0], states[:, 1], states[:, 2], 'b', label='Multi Segment')
+    ax11.scatter(states[0,0], states[0,1], states[0,2], c='g', marker='o')
+    ax11.scatter(states[-1,0], states[-1,1], states[-1,2], c='y', marker='*')
+    
+    Crv_I2R = spice.sxform('MCI','MCR',times)
+    rotatedStates = np.zeros((len(times), 6))
+    for jj in np.arange(len(times)):
+        rotatedStates[jj,:] = Crv_I2R[jj,:,:]@states[jj,:]
 
-ax10.plot(posCRTBP_R_dim[:,0], posCRTBP_R_dim[:,1], posCRTBP_R_dim[:,2], 'r', label='CRTBP')
-plt.legend()
+    ax10.plot(rotatedStates[:, 0], rotatedStates[:, 1], rotatedStates[:, 2], 'b', label='Multi Segment')
+    ax10.scatter(rotatedStates[0,0], rotatedStates[0,1], rotatedStates[0,2], c='g', marker='o')
+    ax10.scatter(rotatedStates[-1,0], rotatedStates[-1,1], rotatedStates[-1,2], c='y', marker='*')
+ax11.plot(posCRTBP_I_dim[:,0], posCRTBP_I_dim[:,1], posCRTBP_I_dim[:,2], 'r-.', label='CRTBP')
+ax11.set_xlabel('X [km]')
+ax11.set_ylabel('Y [km]')
+ax11.set_zlabel('Z [km]')
+ax11.set_title('Moon Centered Inertial Frame')
+plt.legend(loc='upper right', bbox_to_anchor=(1.4, 1), borderaxespad=0)
+
+ax10.plot(posCRTBP_R_dim[:,0], posCRTBP_R_dim[:,1], posCRTBP_R_dim[:,2], 'r-.', label='CRTBP')
+ax10.set_xlabel('X [km]')
+ax10.set_ylabel('Y [km]')
+ax10.set_zlabel('Z [km]')
+ax10.set_title('Moon Centered Rotating Frame')
+plt.legend(loc='upper right', bbox_to_anchor=(1.4, 1), borderaxespad=0)
+
+#ax10 = plt.figure().add_subplot(projection='3d')
+#ax11 = plt.figure().add_subplot(projection='3d')
+#for ii in np.arange(N-1):
+#    Ts = correctedInitialEpoches[ii:ii+2]
+#    times, states = ms.statePropFFR(Ts, correctedInitialStates[ii,:], GM, omega_m)
+#    
+#    ax11.plot(states[:, 0], states[:, 1], states[:, 2], 'b', label='Multi Segment')
+#    ax11.scatter(states[0,0], states[0,1], states[0,2], c='g', marker='o')
+#    ax11.scatter(states[-1,0], states[-1,1], states[-1,2], c='y', marker='*')
+#    
+#    Crv_R2I = spice.sxform('MCR','MCI',times)
+#    inertialStates = np.zeros((len(times), 6))
+#    for jj in np.arange(len(times)):
+#        inertialStates[jj,:] = Crv_R2I[jj,:,:]@states[jj,:]
+#
+#    ax10.plot(inertialStates[:, 0], inertialStates[:, 1], inertialStates[:, 2], 'b', label='Multi Segment')
+#    ax10.scatter(inertialStates[0,0], inertialStates[0,1], inertialStates[0,2], c='g', marker='o')
+#    ax10.scatter(inertialStates[-1,0], inertialStates[-1,1], inertialStates[-1,2], c='y', marker='*')
+#ax10.plot(posCRTBP_I_dim[:,0], posCRTBP_I_dim[:,1], posCRTBP_I_dim[:,2], 'r-.', label='CRTBP')
+#ax10.set_xlabel('X [km]')
+#ax10.set_ylabel('Y [km]')
+#ax10.set_zlabel('Z [km]')
+#ax10.set_title('Moon Centered Inertial Frame')
+#plt.legend(loc='upper right', bbox_to_anchor=(1.4, 1), borderaxespad=0)
+#
+#ax11.plot(posCRTBP_R_dim[:,0], posCRTBP_R_dim[:,1], posCRTBP_R_dim[:,2], 'r-.', label='CRTBP')
+#ax11.set_xlabel('X [km]')
+#ax11.set_ylabel('Y [km]')
+#ax11.set_zlabel('Z [km]')
+#ax11.set_title('Moon Centered Rotating Frame')
+#plt.legend(loc='upper right', bbox_to_anchor=(1.4, 1), borderaxespad=0)
 plt.show()
 breakpoint()