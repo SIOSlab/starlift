4bf7ea36bba30937b68bc9d9d4e8a85c034fb8a0
Grace Genszler <gymnast.q@gmail.com>
2025-08-12 16:55:09 -0400

    Automating the graveyard orbit code for data collection

--- orbits/dataAnalysis.py
+++ orbits/dataAnalysis.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import spiceypy as spice
@@ -7,8 +8,12 @@ import astropy.units as u
 sys.path.insert(1, 'tools')
 import unitConversion
 
-def jacobiConstFFR(state_scM, time):
+def jacobiConstFFR(state_scM, time, GM):
 
+    GM_M = GM[0]
+    GM_E = GM[1]
+    GM_S = GM[2]
+    
     state_baryM = spice.spkezr('Earth Moon Barycenter', time, 'J2000', 'None', 'Moon')[0]
     r_Moon = spice.spkpos('Moon', time, 'J2000', 'None', 'Moon')[0]
     r_Earth = spice.spkpos('Earth', time, 'J2000', 'None', 'Moon')[0]
@@ -22,17 +27,53 @@ def jacobiConstFFR(state_scM, time):
     
     state_scBary = state_scM - state_baryM
     
-    pos_scE = state_scM[0:3] - r_Earth
-    pos_scS = state_scM[0:3] - r_Sun
     pos_Mbary = -state_baryM[0:3]
     pos_Ebary = r_Earth - state_baryM[0:3]
     pos_Sbary = r_Sun - state_baryM[0:3]
     
-    KE1 = np.dot(state_scBary[3:6], state_scBary[3:6])/2
-    U11 = -(state_scBary[0]**2 + state_scBary[1]**2)/2
-    U21 = -(np.linalg.norm(pos_Mbary)/np.linalg.norm(state_scM[0:3]) + np.linalg.norm(pos_Ebary)/np.linalg.norm(pos_scE) + np.linalg.norm(pos_Sbary)/np.linalg.norm(pos_scS))
+    s1 = (pos_Sbary[0] - state_scBary[0])**2
+    s5 = (pos_Sbary[1] - state_scBary[1])**2
+    s4 = (pos_Sbary[2] - state_scBary[2])**2
+        
+    s2 = (pos_Mbary[0] - state_scBary[0])**2
+    s7 = (pos_Mbary[1] - state_scBary[1])**2
+    s6 = (pos_Mbary[2] - state_scBary[2])**2
+    
+    s3 = (pos_Ebary[0] - state_scBary[0])**2
+    s9 = (pos_Ebary[1] - state_scBary[1])**2
+    s8 = (pos_Ebary[2] - state_scBary[2])**2
+    
+    s10 = (pos_Ebary[0] - pos_Mbary[0])**2
+    s11 = (pos_Ebary[1] - pos_Mbary[1])**2
+    s12 = (pos_Ebary[2] - pos_Mbary[2])**2
+    
+    s13 = (pos_Mbary[0] - pos_Sbary[0])**2
+    s14 = (pos_Mbary[1] - pos_Sbary[1])**2
+    s15 = (pos_Mbary[2] - pos_Sbary[2])**2
+    
+    
+    t1 = GM_E/np.sqrt(s9 + s8 + s3)
+    t2 = GM_M/np.sqrt(s7 + s6 + s2)
+    t3 = GM_S/np.sqrt(s5 + s4 + s1)
+    
+    t4 = GM_E*state_scBary[0]*np.sqrt(s10)/(s10 + s11 + s12)**(3/2)
+    t5 = GM_S*state_scBary[0]*np.sqrt(s13)/(s13 + s14 + s15)**(3/2)
 
-    C1 = KE1 + U11 + U21
+    t6 = GM_E*state_scBary[1]*np.sqrt(s11)/(s10 + s11 + s12)**(3/2)
+    t7 = GM_S*state_scBary[1]*np.sqrt(s14)/(s13 + s14 + s15)**(3/2)
+    
+    t8 = GM_E*state_scBary[2]*np.sqrt(s12)/(s10 + s11 + s12)**(3/2)
+    t9 = GM_S*state_scBary[2]*np.sqrt(s15)/(s13 + s14 + s15)**(3/2)
+    
+    Ux = -(-t1 - t2 - t3 - t4 + t5)
+    Uy = -(-t1 - t2 - t3 - t6 + t7)
+    Uz = -(-t1 - t2 - t3 - t8 + t9)
+    
+    KE1 = np.dot(state_scBary[3:6], state_scBary[3:6])/2
+
+    C1 = KE1 + Ux + Uy + Uz
+#    C2 = unitConversion.convertVel_to_canonical(C1*u.km/u.s)
+#    breakpoint()
 
     return C1
 
@@ -58,18 +99,42 @@ gmMoon = spice.bodvrd( 'Moon', 'GM', 1 )[1][0]
 GM = np.array([gmMoon, gmEarth, gmSun])
 #mu_star = gmMoon/(gmEarth + gmMoon)
 
-fileDirectory = '/Users/gracegenszler/Documents/Research/starlift/orbits/graveyardOrbits/L2Nx/'
-#folders = ['6.019359389530944_days/', '6.889032355209055_days/', '7.802188969171071_days/', '8.671861934849183_days/', '9.541534900527276_days/', '10.454691514489271_days/', '11.324364480167363_days/', '12.194037445845456_days/', '13.107194059807451_days/', '13.976867025485543_days/']
-folders = ['6.889032355209055_days/']
+# for each orbit period folder
+baseFileDirectory = '/Users/gracegenszler/Documents/Research/starlift/orbits/graveyardOrbits/L2S/*days/'
+fileDirectories = glob(baseFileDirectory)
+for ii in np.arange(len(fileDirectories)):
+    propTimeMax = 0
+    fileDirMax = ''
+    fileDir_ii = fileDirectories[ii]
+    startTimeDirectories = glob(fileDir_ii + 'startTime_*/')
+    
+    if len(startTimeDirectories) == 0:
+        print('No data. Rerun.')
+        print(fileDir_ii)
+        print('')
+        continue
+        
+    for jj in np.arange(len(startTimeDirectories)):
+        fileDir_jj = startTimeDirectories[jj]
+        attemptDirectories = glob(fileDir_jj + 'attemptStartTime_*')
+        
+        for kk in np.arange(len(attemptDirectories)):
+            propTime = float(attemptDirectories[kk].split("_")[-1][:-4])
+            if propTime > propTimeMax:
+                propTimeMax = propTime
+                fileDirMax = attemptDirectories[kk]
 
-for ii in np.arange(len(folders)):
-    centuryData = np.load(fileDirectory+folders[ii]+'CenturyData.npz')
-    event = np.load(fileDirectory+folders[ii]+'Event.npz')
-    initialFF = np.load(fileDirectory+folders[ii]+'InitialFF.npz')
-    apoluneData = np.load(fileDirectory+folders[ii]+'ApoluneData.npz')
-    perilune = np.load(fileDirectory+folders[ii]+'Perilune.npz')
-    periluneData = np.load(fileDirectory+folders[ii]+'PeriluneData.npz')
-    crtbpData = np.load(fileDirectory+folders[ii]+'CRTBPData.npz', allow_pickle=True)
+    folders = fileDirMax.split('/')
+    print('Orbit period: '+str(round(float(folders[9][:-5]), 3))+' days')
+    print('Start time: '+str(round(float(folders[11].split('_')[1]), 3))+' mjd')
+    print('Prop time: '+str(round(propTimeMax, 3))+' days')
+
+    fileDir_kk = os.path.split(fileDirMax)[0] + '/'
+    initialFF = np.load(fileDir_kk+'InitialFF.npz')
+    apoluneData = np.load(fileDir_kk+'ApoluneData.npz')
+    crtbpData = np.load(fileDir_kk+'CRTBPData.npz', allow_pickle=True)
+    periluneData = np.load(fileDir_kk+'PeriluneData.npz')
+    event = np.load(fileDirMax)
     
     stateApo0 = initialFF['ICs']
     timeApo0 = initialFF['Ts']
@@ -82,11 +147,12 @@ for ii in np.arange(len(folders)):
     patchesApoR = apoluneData['patchStatesR']
     patchesApoI = apoluneData['patchStatesI']
     epochesApo = apoluneData['patchTimes']
-
-    statePeri0 = perilune['ICs']
-    timePeri0 = perilune['Ts']
-    NOrbPeri = perilune['Norbit']
-    NPatchPeri = perilune['Npatch']
+        
+    posCRTBPR = crtbpData['RPs']
+    posCRTBPI = crtbpData['IPs']
+    velCRTBPR = crtbpData['RVs']
+    timesCRTBP = crtbpData['Ts']
+    mu_star = crtbpData['mu_star']
     
     statesPeriR = periluneData['RICs']
     statesPeriI = periluneData['IICs']
@@ -94,134 +160,97 @@ for ii in np.arange(len(folders)):
     patchesPeriR = periluneData['patchStatesR']
     patchesPeriI = periluneData['patchStatesI']
     epochesPeri = periluneData['patchTimes']
-    
-    states100R = centuryData['RICs']
-    states100I = centuryData['IICs']
-    times100 = centuryData['Ts']
-    
-    stateEvent = event['ICs']
-    timeEvent = event['Ts']
-    flagEvent = event['flag']
-    indEvent = np.argwhere(timeEvent == times100)
-        
-    posCRTBPR = crtbpData['RPs']
-    posCRTBPI = crtbpData['IPs']
-    velCRTBPR = crtbpData['RVs']
-    timesCRTBP = crtbpData['Ts']
-    mu_star = crtbpData['mu_star']
 
-    if len(timesApo) > 1:
-        # Original plot
-        ax1 = plt.figure().add_subplot(projection='3d')
-        ax1.plot(statesApoR[:,0], statesApoR[:,1], statesApoR[:,2], 'b', label='Ephemeris Orbit')
-        ax1.plot(posCRTBPR[:,0], posCRTBPR[:,1], posCRTBPR[:,2], 'r-.', label='CRTBP Orbit')
-        ax1.scatter(patchesApoR[:,0], patchesApoR[:,1], patchesApoR[:,2], c='g', marker='o', s=50, alpha=1, label='Patch Points')
-        ax1.set_title('First Multi Shooting Pass')
-        ax1.set_xlabel('x [km]')
-        ax1.set_ylabel('y [km]')
-        ax1.set_zlabel('z [km]')
-        plt.legend()
+    states100I = event['states']
+#    states100R = event['statesR']
+    times100 = event['Ts']
+    
+    states100R = np.zeros((len(times100), 6))
+    for jj in np.arange(len(times100)):
+        Crv_I2R = spice.sxform('MCI','MCR',times100[jj])
+        states100R[jj,:] = Crv_I2R@states100I[jj,:]
 
-        if len(timesPeri) > 1:
-            # Pretty perilune plot
-            ax2 = plt.figure().add_subplot(projection='3d')
-            ax2.plot(statesPeriR[:,0], statesPeriR[:,1], statesPeriR[:,2], 'b', label='Ephemeris Orbit')
-            ax2.plot(posCRTBPR[:,0], posCRTBPR[:,1], posCRTBPR[:,2], 'r-.', label='CRTBP Orbit')
-            ax2.scatter(patchesPeriR[:,0], patchesPeriR[:,1], patchesPeriR[:,2], c='g', marker='o', s=50, alpha=1, label='Patch Points')
-            ax2.set_title('Multi Segment Ephemeris Orbit')
-            ax2.set_xlabel('x [km]')
-            ax2.set_ylabel('y [km]')
-            ax2.set_zlabel('z [km]')
-            plt.legend()
+    # Original plot
+    ax1 = plt.figure().add_subplot(projection='3d')
+    ax1.plot(statesApoR[:,0], statesApoR[:,1], statesApoR[:,2], 'b', label='Ephemeris Orbit')
+    ax1.plot(posCRTBPR[:,0], posCRTBPR[:,1], posCRTBPR[:,2], 'r-.', label='CRTBP Orbit')
+    ax1.scatter(patchesApoR[:,0], patchesApoR[:,1], patchesApoR[:,2], c='g', marker='o', s=50, alpha=1, label='Patch Points')
+    ax1.set_title('First Multi Shooting Pass')
+    ax1.set_xlabel('x [km]')
+    ax1.set_ylabel('y [km]')
+    ax1.set_zlabel('z [km]')
+    plt.legend()
+
+    # Pretty perilune plot
+    ax2 = plt.figure().add_subplot(projection='3d')
+    ax2.plot(statesPeriR[:,0], statesPeriR[:,1], statesPeriR[:,2], 'b', label='Ephemeris Orbit')
+    ax2.plot(posCRTBPR[:,0], posCRTBPR[:,1], posCRTBPR[:,2], 'r-.', label='CRTBP Orbit')
+    ax2.scatter(patchesPeriR[:,0], patchesPeriR[:,1], patchesPeriR[:,2], c='g', marker='o', s=50, alpha=1, label='Patch Points')
+    ax2.set_title('Multi Segment Ephemeris Orbit')
+    ax2.set_xlabel('x [km]')
+    ax2.set_ylabel('y [km]')
+    ax2.set_zlabel('z [km]')
+    plt.legend()
             
-            if np.any(indEvent):
-                indEvent = indEvent[0,0]
-                # Event plot
-                ax3 = plt.figure().add_subplot(projection='3d')
-                ax3.plot(states100R[:indEvent,0], states100R[:indEvent,1], states100R[:indEvent,2], 'b', label='Ephemeris Orbit')
-                ax3.plot(posCRTBPR[:,0], posCRTBPR[:,1], posCRTBPR[:,2], 'r-.', label='CRTBP Orbit')
-                ax3.set_title(str((times100[indEvent]-times100[0])/60/60/24)+' day propagation')
-                ax3.set_xlabel('x [km]')
-                ax3.set_ylabel('y [km]')
-                ax3.set_zlabel('z [km]')
-                plt.legend()
-                
-                # Century plot, if applicable
-                ax4 = plt.figure().add_subplot(projection='3d')
-                ax4.plot(states100R[:,0], states100R[:,1], states100R[:,2], 'b', label='Ephemeris Orbit')
-                ax4.set_title('Moon Centered Rotating Frame: Propogate for 100 years')
-                ax4.set_xlabel('x [km]')
-                ax4.set_ylabel('y [km]')
-                ax4.set_zlabel('z [km]')
-                plt.legend()
-                
-                ax5 = plt.figure().add_subplot(projection='3d')
-                ax5.plot(states100I[:,0], states100I[:,1], states100I[:,2], 'b', label='Ephemeris Orbit')
-                ax5.set_title('Moon Centered Inertial Frame: Propogate for 100 years')
-                ax5.set_xlabel('x [km]')
-                ax5.set_ylabel('y [km]')
-                ax5.set_zlabel('z [km]')
-                plt.legend()
-                
-#                CFF = np.array([])
-#                ctr = 0
-#                while (times100[ctr] - times100[0])/24/60/60 < 7*24*60*60:
-#                    tmp = jacobiConstFFR(states100R[ctr,:], times100[ctr])
-#                    CFF = np.append(CFF, tmp)
-#                    ctr = ctr + 1
-                    
-                # Jacobi constant with ephemeris data, but CRTBP calculation
-#                CFF = np.zeros(len(times100[:indEvent]))
-#                for jj in np.arange(len(times100[:indEvent])):
-#                    states100R[jj,0:3]*u.km
-#                    pos_can = unitConversion.convertPos_to_canonical(states100R[jj,0:3]*u.km) + np.array([1-mu_star, 0, 0])
-#                    vel_can = unitConversion.convertVel_to_canonical(states100R[jj,3:6]*u.km/u.s)
-#                    CFF[jj] = jacobiConstCRTBPR(pos_can, vel_can, mu_star)
-                CFF = np.zeros(len(times100))
-                for jj in np.arange(len(times100)):
-                    states100R[jj,0:3]*u.km
-                    pos_can = unitConversion.convertPos_to_canonical(states100R[jj,0:3]*u.km) + np.array([1-mu_star, 0, 0])
-                    vel_can = unitConversion.convertVel_to_canonical(states100R[jj,3:6]*u.km/u.s)
-                    CFF[jj] = jacobiConstCRTBPR(pos_can, vel_can, mu_star)
+    # Event plot
+    ax3 = plt.figure().add_subplot(projection='3d')
+    ax3.plot(states100R[:,0], states100R[:,1], states100R[:,2], 'b', label='Ephemeris Orbit')
+    ax3.plot(posCRTBPR[:,0], posCRTBPR[:,1], posCRTBPR[:,2], 'r-.', label='CRTBP Orbit')
+    ax3.set_title('MCR Frame '+str(round(propTimeMax, 3))+' day propagation')
+    ax3.set_xlabel('x [km]')
+    ax3.set_ylabel('y [km]')
+    ax3.set_zlabel('z [km]')
+    plt.legend()
 
-                # Jacobi constant CRTBP
-                CCRTBP = np.zeros(len(timesCRTBP))
-                timesCRTBP_dim = np.array([])
-                for jj in np.arange(len(timesCRTBP)):
-                    pos_can = unitConversion.convertPos_to_canonical(posCRTBPR[jj,:]*u.km) + np.array([1-mu_star, 0, 0])
-                    vel_can = unitConversion.convertVel_to_canonical(velCRTBPR[jj,:]*u.km/u.s)
-                    CCRTBP[jj] = jacobiConstCRTBPR(pos_can, vel_can, mu_star)
-                    timesCRTBP_dim = np.append(timesCRTBP_dim, timesCRTBP[jj].value)
-                timesCRTBP_dim = (timesCRTBP_dim - timesCRTBP_dim[0])
-
-#                times100plt = times100[:indEvent] - times100[0]
-                times100plt = times100 - times100[0]
-#                breakpoint()
-                plt.figure(6)
-#                plt.plot(times100plt, CFF[:indEvent], label = 'FF')
-                plt.plot(times100plt, CFF, label = 'FF')
-                plt.plot(timesCRTBP_dim, CCRTBP, label = 'CRTBP')
-                plt.xlabel('time [days]')
-                plt.ylabel('Jacobi constant [nd]')
-                plt.legend()
-                plt.xlim(0,7)
-                plt.ylim(-1.525,-1.515)
-                plt.show()
-                breakpoint()
-                if flagEvent == 0:
-                    print('Crashes into Moon')
-                elif flagEvent == 1:
-                    print('Crashes into Earth')
-                elif flagEvent == 2:
-                    print('Crashes into Sun')
-                elif flagEvent == 3:
-                    print('Orbit loses shape')
-            else:
-                print('No event')
-        else:
-            print('No perilune data')
-    else:
-        print('No data')
+    ax4 = plt.figure().add_subplot(projection='3d')
+    ax4.plot(states100I[:,0], states100I[:,1], states100I[:,2], 'b', label='Ephemeris Orbit')
+    ax4.plot(posCRTBPI[:,0], posCRTBPI[:,1], posCRTBPI[:,2], 'r-.', label='CRTBP Orbit')
+    ax4.set_title('MCI Frame '+str(round(propTimeMax, 3))+' day propagation')
+    ax4.set_xlabel('x [km]')
+    ax4.set_ylabel('y [km]')
+    ax4.set_zlabel('z [km]')
+    plt.legend()
         
-    plt.show()
-    breakpoint()
+    # Jacobi constant with FF
+    CFF = np.zeros(len(times100))
+    CFF2 = np.zeros(len(times100))
+    for jj in np.arange(len(times100)):
+        states100R[jj,0:3]*u.km
+        pos_can = unitConversion.convertPos_to_canonical(states100R[jj,0:3]*u.km) + np.array([1-mu_star, 0, 0])
+        vel_can = unitConversion.convertVel_to_canonical(states100R[jj,3:6]*u.km/u.s)
+        CFF[jj] = jacobiConstCRTBPR(pos_can, vel_can, mu_star)
+        CFF2[jj] = jacobiConstFFR(states100R[jj,:], times100[jj], GM)
+    CFF2 = unitConversion.convertVel_to_canonical(CFF2*u.km/u.s)
+    
+    # Jacobi constant CRTBP
+    CCRTBP = np.zeros(len(timesCRTBP))
+    timesCRTBP_dim = np.array([])
+    for jj in np.arange(len(timesCRTBP)):
+        pos_can = unitConversion.convertPos_to_canonical(posCRTBPR[jj,:]*u.km) + np.array([1-mu_star, 0, 0])
+        vel_can = unitConversion.convertVel_to_canonical(velCRTBPR[jj,:]*u.km/u.s)
+        CCRTBP[jj] = jacobiConstCRTBPR(pos_can, vel_can, mu_star)
+        timesCRTBP_dim = np.append(timesCRTBP_dim, timesCRTBP[jj].value)
+    timesCRTBP_dim = (timesCRTBP_dim - timesCRTBP_dim[0])
+    
+    plt.figure(6)
+    plt.plot(times100, CFF2, label = 'FF - FF Method')
+    plt.plot(times100, CFF, label = 'FF - CRTBP Method')
+    plt.plot(timesCRTBP_dim+times100[0], CCRTBP, label = 'CRTBP')
+    plt.xlabel('time [days]')
+    plt.ylabel('Jacobi constant [nd]')
+    plt.legend()
+#    plt.xlim(0,7)
+#    plt.ylim(-1.525,-1.515)
+
+    
+#    if flagEvent == 0:
+#        print('Crashes into Moon \n')
+#    elif flagEvent == 1:
+#        print('Crashes into Earth \n')
+#    elif flagEvent == 2:
+#        print('Crashes into Sun \n')
+#    elif flagEvent == 3:
+#        print('Orbit loses shape \n')
+    
+#    plt.show()
+#    breakpoint()
--- orbits/ephemerisModelForced.py
+++ orbits/ephemerisModelForced.py
@@ -43,19 +43,14 @@ Tp_m = spice.oscltx(rvMoon, et_start, gmEarth)[-1]
 omega_m = 2*np.pi/Tp_m
 
 # Initial condition in canonical units in rotating frame R [pos, vel, time, U]
-fileStr = 'TrajI_1265'
+fileStr = 'TrajExample'
+#fileStr = 'TrajI_1265'
 mat_data = loadmat(fileStr+'.mat')['TrajI']
-mat_data2 = loadmat('TrajExample.mat')['TrajI']
 posCRTBP_R = mat_data[:,0:3]
 velCRTBP_R = mat_data[:,3:6]
 timesCRTBP_R = mat_data[:,6]
 uT = mat_data[:,7:]
 mu_cstar = 0.01215059
-
-posCRTBP_R2 = mat_data2[:,0:3]
-velCRTBP_R2 = mat_data2[:,3:6]
-timesCRTBP_R2 = mat_data2[:,6]
-uT2 = mat_data2[:,7:]
 
 # Convert from nondimensional units to dimensional
 posCRTBP_R_dim = unitConversion.convertPos_to_dim(posCRTBP_R - np.array([1-mu_cstar, 0, 0])).to_value(u.km)
@@ -65,23 +60,11 @@ timesCRTBP_mjd = t_start + timesCRTBP_d
 etCRTBP_mjd = spice.str2et(timesCRTBP_mjd.iso)
 uT_dim = unitConversion.convertAcc_to_dim(uT).to('km/s**2')
 
-posCRTBP_R_dim2 = unitConversion.convertPos_to_dim(posCRTBP_R2 - np.array([1-mu_cstar, 0, 0])).to_value(u.km)
-velCRTBP_R_dim2 = unitConversion.convertVel_to_dim(velCRTBP_R2).to_value(u.km/u.s)
-timesCRTBP_d2 = unitConversion.convertTime_to_dim(timesCRTBP_R2).to('d')
-timesCRTBP_mjd2 = t_start + timesCRTBP_d2
-etCRTBP_mjd2 = spice.str2et(timesCRTBP_mjd2.iso)
-uT_dim2 = unitConversion.convertAcc_to_dim(uT2).to('km/s**2')
-
 # Calculate delta-v
 uT_mag = np.linalg.norm(uT_dim, axis=1)
 dVCRTBPtot = cumulative_trapezoid(uT_mag, x=etCRTBP_mjd, axis=0)*u.km/u.s
 dVCRTBP = np.append(0*u.km/u.s,dVCRTBPtot)
 dVCRTBP = np.diff(dVCRTBP)
-
-uT_mag2 = np.linalg.norm(uT_dim2, axis=1)
-dVCRTBPtot2 = cumulative_trapezoid(uT_mag2, x=etCRTBP_mjd2, axis=0)*u.km/u.s
-dVCRTBP2 = np.append(0*u.km/u.s,dVCRTBPtot2)
-dVCRTBP2 = np.diff(dVCRTBP2)
 
 # Calculate mass profile
 Isp = 1500*u.s                                          # seconds
@@ -91,13 +74,8 @@ g0 = const.g0.value*const.g0.unit                     # m/s^2
 mf = mi*np.exp(-dVCRTBPtot/(Isp*g0))   # kg
 m_dim = np.append(mi, mf)                               # mass history
 
-Ftmax2 = (max(uT_mag2)*mi)                       # mN
-mf2 = mi*np.exp(-dVCRTBPtot2/(Isp*g0))   # kg
-m_dim2 = np.append(mi, mf2)                               # mass history
-
 # Calculate the force profile
 Ft_mag = (uT_mag*m_dim).to_value(u.mN)
-Ft_mag2 = (uT_mag2*m_dim2).to_value(u.mN)
 
 posCRTBP_I_dim = np.zeros((len(etCRTBP_mjd), 3))
 for ii in np.arange(len(etCRTBP_mjd)):
@@ -183,8 +161,7 @@ for ii in np.arange(N-1):
 
     rotatedStates = np.vstack((rotatedStates, rStates[:-1,:]))
     timesTot = np.append(timesTot, times[:-1])
-    breakpoint()
-
+    
 diff = rotatedStates[-1] - rotatedStates[1]
 rDiff = np.linalg.norm(diff[0:3])
 vDiff = np.linalg.norm(diff[3:6])
@@ -239,7 +216,6 @@ plt.yscale('log')
 plt.xlabel('Time since epoch [s]')
 plt.ylabel('Control Force [mN]')
 plt.title('Thrust History')
-plt.show()
 
 filepath = '/Users/gracegenszler/Documents/Research/starlift/orbits/forcedOrbits/'+fileStr
 if os.path.isdir(filepath):
@@ -248,116 +224,6 @@ else:
     os.makedirs(filepath)
     
 np.savez(filepath+'/InitialFF.npz', ICs = correctedInitialStates, FCs = correctedFinalStates, Ts = correctedInitialEpoches, times = timesTot, statesR = rotatedStates, statesI = inertialStates, Npatch = N, dVpatches = dVtot, startTime = t_start, mu_star = mu_cstar)
-breakpoint()
 
-
-# save things
-# Convert impulsive burns into continuous thrust
-sigma = 1
-inds = np.array([])
-dts = np.array([])
-Ups = np.array([])
-for ii in np.arange(1,len(correctedInitialEpoches)-2):
-    uT_flag = False
-    timeDiff1 = np.abs(etCRTBP_mjd - correctedInitialEpoches[ii])
-    
-    ind_ii = np.argwhere(timeDiff1 == min(timeDiff1))[0,0]
-    uT_ii = uT_mag[ind_ii]
-    while not uT_flag:
-        # get uT at patch point and estimate dt for a burn with a buffer
-        dt_ii = dVtot[ii]*u.km/u.s/(Ftmax/mf[ii]*sigma - uT_ii)
-        if dt_ii < 0:
-            breakpoint()
-        # check the uT in the dt range, centered at patch point
-        
-        t_initial = correctedInitialEpoches[ii] - dt_ii.value/2
-        t_final = correctedInitialEpoches[ii] + dt_ii.value/2
-        
-        timeDiff2 = np.abs(etCRTBP_mjd - t_initial)
-        timeDiff3 = np.abs(etCRTBP_mjd - t_final)
-        ind_i = np.argwhere(timeDiff2 == min(timeDiff2))[0,0]
-        ind_f = np.argwhere(timeDiff3 == min(timeDiff3))[0,0]
-        
-        if ind_i != ind_f:
-            uTcheck = np.argwhere(uT_mag[ind_i] + dVtot[ii]*u.km/u.s/dt_ii > Ftmax/mf[ii])
-        else:
-            uTcheck = np.array([])
-
-        if np.any(uTcheck):
-            # rescale time based off of max thrust force in current time interval
-            uT_ii = max(uT_mag[ind_i:ind_f])
-        else:
-            # save info to create continuous burn
-            inds = np.append(inds, np.array([ind_i, ind_f]))
-            dts = np.append(dts, dt_ii.value)
-            Ups = np.append(Ups, uT_ii.value)
-            uT_flag = True
-           
-        print(ind_i)
-        print(ind_f)
-        print(dt_ii)
-
-# create a new uT array from scratch
-dStates = correctedFinalStates[:,3:6] - correctedInitialStates[:-1,3:6]
-
-t_min = correctedInitialEpoches[1] - dts[0]/2
-t_max = correctedInitialEpoches[1] + dts[0]/2
-ind_min = np.argwhere(etCRTBP_mjd < t_min)[-1,0]
-ind_old = np.argwhere(etCRTBP_mjd > t_max)[0,0]
-uT_new = uT_dim[:,:ind_min].copy().value
-uT_burn = Ups[0]*dStates[0]/np.linalg.norm(dStates[0])
-etCRTBP_mjd_new = etCRTBP_mjd[:ind_min].copy()
-
-if ind_min != ind_old:
-    uT_patch = uT_burn + uT_dim[ind_min+1:ind_old,:][0,:].value
-else:
-    uT_patch = uT_burn + uT_dim[ind_min,:].value
-    
-uT_patch = np.reshape(np.repeat(uT_patch, repeats=3,axis=0),(3,3)).T
-uT_new = np.vstack((uT_new,uT_patch))
-etCRTBP_mjd_new = np.append(etCRTBP_mjd_new, np.array([t_min, correctedInitialEpoches[1], t_max]))
-    
-for ii in np.arange(1,len(dts)):
-    t_min = correctedInitialEpoches[ii+1] - dts[ii]/2
-    t_max = correctedInitialEpoches[ii+1] + dts[ii]/2
-    ind_min = np.argwhere(etCRTBP_mjd < t_min)[-1,0]
-    ind_max = np.argwhere(etCRTBP_mjd > t_max)[0,0]
-    uT_burn = Ups[ii]*dStates[ii]/np.linalg.norm(dStates[ii])
-    
-    if ind_max - ind_min > 1:
-        uT_patch = uT_burn + uT_dim[ind_min+1:ind_max,:][0,:].value
-    elif ind_max - ind_min == 1:
-        uT_patch = uT_burn + (uT_dim[ind_min,:]+uT_dim[ind_max,:]).value/2
-    else:
-        uT_patch = uT_burn + uT_dim[ind_min,:].value
-    uT_patch = np.reshape(np.repeat(uT_patch, repeats=3,axis=0),(3,3)).T
-    uT_new = np.vstack((uT_new,uT_patch))
-    etCRTBP_mjd_new = np.append(etCRTBP_mjd_new, np.array([t_min, correctedInitialEpoches[ii+1], t_max]))
-    
-    ind_old = np.argwhere(etCRTBP_mjd > t_max)[0,0]
-uT_new = np.vstack((uT_new, uT_dim[ind_old:,:].value))
-etCRTBP_mjd_new = np.append(etCRTBP_mjd_new, etCRTBP_mjd[ind_old:])
-
-Ts = np.array([correctedInitialEpoches[0], correctedInitialEpoches[-1]])
-times_final, states_final = ms.statePropFFIForced(Ts, correctedInitialStates[0,:], GM, uT_new, etCRTBP_mjd_new)
-
-states_final_R = np.zeros((len(times_final), 6))
-for ii in np.arange(len(times_final)):
-    Crv_I2R = spice.sxform('MCI','MCR',times_final[ii])
-    states_final_R[ii,:] = Crv_I2R@states_final[ii,:]
-    
-
-ax12 = plt.figure().add_subplot(projection='3d')
-ax12.plot(rotatedStates[:, 0], rotatedStates[:, 1], rotatedStates[:, 2], 'b', label='Multi Segment')
-ax12.plot(posCRTBP_R_dim[:,0], posCRTBP_R_dim[:,1], posCRTBP_R_dim[:,2], 'r-.', label='CRTBP')
-ax12.plot(states_final_R[:,0], states_final_R[:,1], states_final_R[:,2], 'g', label='Final Trajectory')
-ax12.set_xlabel('X [km]')
-ax12.set_ylabel('Y [km]')
-ax12.set_zlabel('Z [km]')
-ax12.set_title('Patch Points in Moon Centered Rotating Frame')
 plt.show()
 breakpoint()
-
-
-# separate into two files
-# fix the repeat in added data
